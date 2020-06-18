04fa8fe92cd9bba6db4d0199b7b9a53dca9626bc
wissensalt212 <fauzi.knightmaster.achmad@gmail.com>
2020-06-18 22:29:03 +0700

    init db + logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wissensalt/wissensalt-go-util/db"
+	"github.com/wissensalt/wissensalt-go-util/logging"
+)
 
 func main()  {
+	//init logger
+	logging.Init()
+	logging.AppLogger.Println("Hello World")
+
+	//init db connection
+	dbConnection := new(db.ConnectionProperty)
+	dbConnection.Init()
+	var connectionService db.ConnectionService = dbConnection
+	connectionService.ConnectMySql()
+
+	// init router
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
