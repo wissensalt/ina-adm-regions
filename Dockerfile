f37b7f367af2163887df9d4ea79a9e684758c2d1
wissensalt212 <fauzi.knightmaster.achmad@gmail.com>
2020-06-18 12:09:05 +0700

    add multiple stage build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,35 @@
-FROM golang:latest
+FROM golang:alpine AS builder
 
-COPY main.go /app/main.go
+# Set necessary environmet variables needed for our image
+ENV GO111MODULE=on \
+    CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64
 
-CMD ["go", "run", "/app/main.go"]
+# Move to working directory /build
+WORKDIR /build
+
+# Copy and download dependency using go mod
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+
+# Copy the code into the container
+COPY . .
+
+# Build the application
+RUN go build -o main .
+
+# Move to /dist directory as the place for resulting binary folder
+WORKDIR /dist
+
+# Copy binary from build to main folder
+RUN cp /build/main .
+
+# Build a small image
+FROM scratch
+
+COPY --from=builder /dist/main /
+
+# Command to run
+ENTRYPOINT ["/main"]
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 run_mysql_and_service_and_migration() {
     run_mysql_and_service_without_migration
     run_migration_migrate
@@ -29,29 +30,41 @@ run_migration_down() {
     echo "Finished Rollback migration version : "$version
 }
 
+run_download_dependencies() {
+    go mod init ${PWD}
+    go mod download
+}
+
 echo "\n===============Indonesia Administrative Regions===============\n"
 
 echo "1. Run All (Mysql, Service without Migration)"
 echo "2. Run Migration UP"
 echo "3. Run Migration Down"
 echo "4. Run All (Mysql, Service with Migration)"
+echo "5. Download dependencies (create go.mod and go.sum)"
 
 read -p "Pick options : " option
 
-if [ $option == 1 ]
+if [[ ${option} == 1 ]]
 then
+    run_download_dependencies
     run_mysql_and_service_without_migration
-elif [ $option == 2 ]
+elif [[ ${option} == 2 ]]
 then
     run_migration_up
-elif [ $option == 3 ]
+elif [[ ${option} == 3 ]]
 then
     run_migration_down
-elif [ $option == 4 ]
+elif [[ ${option} == 4 ]]
 then
+    run_download_dependencies
     run_mysql_and_service_without_migration
     run_migration_up
+elif [[ ${option} == 5 ]]
+then
+    run_download_dependencies
 else
+    run_download_dependencies
     run_mysql_and_service_without_migration
     run_migration_up
 fi
